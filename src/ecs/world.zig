d3b644eae93ce625590399764dd9607f5c7bc164
LeviathanST <phamquochung170505@gmail.com>
2025-10-31 22:56:12 +0700

    ecs: add & run systems, add `getMutComponent`, change params & some logic of `getComponent`
    * `getComponent`: get storage in std.HashMap via `getOrPut` -> `get`,
    because we need to init some fields in the storage.
    
    * `getMutComponent`: get mutable component.
    
    * `systems`

--- src/ecs/world.zig
+++ src/ecs/world.zig
@@ -6,6 +6,7 @@ const ComponentStorage = component.Storage;
 const World = @This();
 
 pub const EntityID = usize;
+pub const SystemFn = *const fn (World) anyerror!void;
 
 entity_count: usize = 0,
 /// Each storage store data of a component.
@@ -18,6 +19,7 @@ entity_count: usize = 0,
 ///
 /// Get data of a component via `get('name_of_your_component')`
 component_storages: std.StringHashMap(ErasedComponentStorage),
+systems: std.ArrayList(SystemFn),
 alloc: std.mem.Allocator,
 _arena: *std.heap.ArenaAllocator,
 
@@ -28,6 +30,7 @@ pub fn init(alloc: std.mem.Allocator) World {
     return .{
         .alloc = arena.allocator(),
         .component_storages = .init(alloc),
+        .systems = .empty,
         ._arena = arena,
     };
 }
@@ -37,6 +40,9 @@ pub fn deinit(self: *World) void {
     while (storage_iter.next()) |entry| {
         entry.value_ptr.*.deinit_fn(entry.value_ptr.ptr, self.alloc);
     }
+
+    self.component_storages.deinit();
+    self.systems.deinit(self.alloc);
 
     self._arena.deinit();
     self._arena.child_allocator.destroy(self._arena);
@@ -57,6 +63,7 @@ pub fn newComponentStorage(
     comptime T: type,
 ) void {
     const storage = self.alloc.create(ComponentStorage(T)) catch @panic("OOM");
+    errdefer self.alloc.destroy(storage);
     storage.* = .{
         .data = .empty,
     };
@@ -76,19 +83,16 @@ pub fn newComponentStorage(
 pub fn setComponent(
     self: *World,
     entity_id: EntityID,
+    comptime T: type,
     component_name: []const u8,
-    component_value: anytype,
-) void {
+    component_value: T,
+) !void {
     // Get the storage of `T` component by `name`.
     const r = self
         .component_storages
-        // Create the new storage if not exists.
-        .getOrPut(component_name) catch @panic("OOM");
+        .get(component_name) orelse return error.ComponentStorageNotFound;
 
-    const actual_s = ErasedComponentStorage.cast(
-        r.value_ptr.ptr,
-        @TypeOf(component_value),
-    );
+    const actual_s = ErasedComponentStorage.cast(r.ptr, T);
 
     // Append the value of the component to data
     // list in the storage
@@ -99,13 +103,31 @@ pub fn setComponent(
     ) catch @panic("OOM");
 }
 
-pub fn getComponent(self: *World, entity_id: EntityID, name: []const u8, comptime T: type) !T {
+pub fn getComponent(
+    self: World,
+    entity_id: EntityID,
+    name: []const u8,
+    comptime T: type,
+) !T {
     const s = self
         .component_storages
         .get(name) orelse return error.ComponentStorageNotFound;
 
     const actual_s = ErasedComponentStorage.cast(s.ptr, T);
     return actual_s.data.get(entity_id) orelse error.ComponentValueNotFound;
+}
+pub fn getMutComponent(
+    self: World,
+    entity_id: EntityID,
+    name: []const u8,
+    comptime T: type,
+) !*T {
+    const s = self
+        .component_storages
+        .get(name) orelse return error.ComponentStorageNotFound;
+
+    const actual_s = ErasedComponentStorage.cast(s.ptr, T);
+    return actual_s.data.getPtr(entity_id) orelse error.ComponentValueNotFound;
 }
 
 test "Init entities" {
@@ -118,20 +140,29 @@ test "Init entities" {
 
     var world: World = .init(alloc);
     defer world.deinit();
-
     world.newComponentStorage("position", Position);
 
     const entity_1 = world.newEntity();
-    world.setComponent(entity_1, "position", Position{ .x = 5, .y = 6 });
+    try world.setComponent(entity_1, Position, "position", .{ .x = 5, .y = 6 });
 
     const comp_value_1 = try world.getComponent(entity_1, "position", Position);
     try std.testing.expect(comp_value_1.x == 5);
     try std.testing.expect(comp_value_1.y == 6);
 
     const entity_2 = world.newEntity();
-    world.setComponent(entity_2, "position", Position{ .x = 10, .y = 6 });
+    try world.setComponent(entity_2, Position, "position", .{ .x = 10, .y = 6 });
 
     const comp_value_2 = try world.getComponent(entity_2, "position", Position);
     try std.testing.expect(comp_value_2.x == 10);
     try std.testing.expect(comp_value_2.y == 6);
+}
+
+pub fn addSystem(self: *World, system: SystemFn) void {
+    self.systems.append(self.alloc, system) catch @panic("OOM");
+}
+
+pub fn run(self: World) !void {
+    for (self.systems.items) |system| {
+        try system(self);
+    }
 }
