44ebd173ab152cf4d79eef120dd0e886fb24e8ab
LeviathanST <phamquochung170505@gmail.com>
2025-10-31 19:59:32 +0700

    Digger, Grid: remove some fields to reduce footprint memory

--- src/Digger.zig
+++ src/Digger.zig
@@ -9,20 +9,19 @@ const Digger = @This();
 col: i32,
 /// the row index in the grid
 row: i32,
-_grid: Grid,
 
-pub fn init(row: i32, col: i32, grid: Grid) !Digger {
-    return .{ .col = col, .row = row, ._grid = grid };
+pub fn init(row: i32, col: i32) Digger {
+    return .{ .col = col, .row = row };
 }
 
-pub fn move(self: *Digger, kinds: enum { up, down, left, right }) void {
+pub fn move(self: *Digger, grid: Grid, kinds: enum { up, down, left, right }) void {
     switch (kinds) {
         .up => {
             if (self.row - 1 >= 0)
                 self.row -= 1;
         },
         .down => {
-            if (self.row + 1 < self._grid.num_of_rows)
+            if (self.row + 1 < grid.num_of_rows)
                 self.row += 1;
         },
         .left => {
@@ -30,14 +29,14 @@ pub fn move(self: *Digger, kinds: enum { up, down, left, right }) void {
                 self.col -= 1;
         },
         .right => {
-            if (self.col + 1 < self._grid.num_of_cols)
+            if (self.col + 1 < grid.num_of_cols)
                 self.col += 1;
         },
     }
 }
 
-pub fn draw(self: Digger) !void {
-    const pos = self._grid.matrix[@intCast(try self._grid.getActualIndex(self.row, self.col))];
+pub fn draw(self: Digger, grid: Grid) !void {
+    const pos = grid.matrix[@intCast(try grid.getActualIndex(self.row, self.col))];
     const pos_x = pos.x + @divTrunc(pos.width, 2);
     const pos_y = pos.y + @divTrunc(pos.width, 2);
     rl.drawCircle(pos_x, pos_y, 10, .red);
