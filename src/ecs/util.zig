ba77850916c9672b175fa0ebfb1ee27f55136b70
LeviathanST <phamquochung170505@gmail.com>
2025-11-04 19:17:08 +0700

    ecs/util: reformat code

--- src/ecs/util.zig
+++ src/ecs/util.zig
@@ -2,7 +2,5 @@ const std = @import("std");
 
 /// Return the child type of `T` if it is a pointer.
 pub fn Deref(comptime T: type) type {
-    if (@typeInfo(T) == .pointer)
-        return std.meta.Child(T);
-    return T;
+    return if (@typeInfo(T) == .pointer) std.meta.Child(T) else T;
 }
