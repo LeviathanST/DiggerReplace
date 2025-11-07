8345b41eafad91e70cf50819bcc435418bfa2fe4
LeviathanST <phamquochung170505@gmail.com>
2025-11-07 22:27:33 +0700

    game: change the entry point of each feature to `mod.zig` as a module

--- src/features/area/mod.zig
+++ src/features/area/mod.zig
@@ -1,5 +1,13 @@
+const systems = @import("systems.zig");
+
 const World = @import("ecs").World;
 const Grid = @import("shared_components").Grid;
+
+pub fn build(w: *World) void {
+    _ = w
+        .addSystem(.startup, spawn)
+        .addSystems(.update, &.{systems.render});
+}
 
 pub fn spawn(w: *World) !void {
     w.spawnEntity(
