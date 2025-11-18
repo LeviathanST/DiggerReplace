0867ea49f302a192021f66bacf204914be0b950a
LeviathanST <phamquochung170505@gmail.com>
2025-11-18 20:05:21 +0700

    ecs: remove unnecessary

--- src/ecs.zig
+++ src/ecs.zig
@@ -2,9 +2,11 @@ const component = @import("ecs/component.zig");
 
 pub const ErasedComponentStorage = component.ErasedStorage;
 pub const ComponentStorage = component.Storage;
+
 pub const World = @import("ecs/World.zig");
 
-pub const mods = @import("ecs/modules.zig");
+pub const common = @import("ecs/common.zig");
+pub const CommonModule = common.CommonModule;
 
 test {
     @import("std").testing.refAllDecls(@This());
