535326f6c60e72a5ba702d56fac8a5cf4e41f19c
LeviathanST <phamquochung170505@gmail.com>
2025-11-18 20:52:50 +0700

    main/area: remove `Grid` & add `Area` component

--- src/features/area/systems.zig
+++ src/features/area/systems.zig
@@ -3,31 +3,24 @@ const rl = @import("raylib");
 
 const GameAssets = @import("../../GameAssets.zig");
 const World = @import("ecs").World;
-const Grid = @import("components.zig").Grid;
+const Grid = @import("ecs").common.Grid;
+const Area = @import("components.zig").Area;
 
 pub fn render(w: *World, _: std.mem.Allocator) !void {
     const assets = try w.getMutResource(GameAssets);
-    const queries = try w.query(&.{Grid});
+    const queries = try w.query(&.{ Grid, Area });
     const font = try assets.getMainFont();
 
     for (queries) |query| {
         const grid = query[0]; // get "grid" field
 
         for (grid.matrix, 0..) |cell, i| {
-            rl.drawRectangle(
-                @intCast(cell.x),
-                @intCast(cell.y),
-                @intCast(cell.width),
-                @intCast(cell.width),
-                .blue,
-            );
-
             rl.drawTextEx(
                 font,
                 rl.textFormat("%d", .{i}),
                 .{
-                    .x = @floatFromInt(cell.x + @divTrunc(cell.width, 2) - 5),
-                    .y = @floatFromInt(cell.y + @divTrunc(cell.width, 2) - 5),
+                    .x = @floatFromInt(cell.x + @divTrunc(grid.cell_width, 2) - 5),
+                    .y = @floatFromInt(cell.y + @divTrunc(grid.cell_width, 2) - 5),
                 },
                 @floatFromInt(font.baseSize - 9),
                 0,
