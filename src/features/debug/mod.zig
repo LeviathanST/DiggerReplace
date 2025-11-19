b30d280afa5725569b4f77b6b0767626e2bc4611
LeviathanST <phamquochung170505@gmail.com>
2025-11-19 15:47:43 +0700

    ingame/debug: remove unnecessary
    i wrote something to test but i forgot to remove it :( im sorry.

--- src/features/debug/mod.zig
+++ src/features/debug/mod.zig
@@ -2,13 +2,11 @@
 const std = @import("std");
 const rl = @import("raylib");
 const systems = @import("systems.zig");
-const ecs_common = @import("ecs").common;
 const components = @import("components.zig");
 
 const World = @import("ecs").World;
-const Position = ecs_common.Position;
-const Box = ecs_common.DebugBox;
-const Info = ecs_common.DebugInfo;
+const Box = components.DebugBox;
+const Info = components.DebugInfo;
 
 pub fn build(w: *World) void {
     _ = w
@@ -21,14 +19,12 @@ pub fn build(w: *World) void {
 
 pub fn spawn(w: *World, _: std.mem.Allocator) !void {
     w.spawnEntity(
-        &.{ Info, Position, Box },
+        &.{ Info, Box },
         .{
             .{},
             .{
                 .x = 10,
                 .y = rl.getScreenHeight() - 100,
-            },
-            .{
                 .font_size = 20,
                 .width = 250,
                 .item_height = 30,
