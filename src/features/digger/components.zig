f18d05e42812478df2546a8854b03e497bd9b207
LeviathanST <phamquochung170505@gmail.com>
2025-11-19 17:06:29 +0700

    ingame/digger: use `circle`

--- src/features/digger/components.zig
+++ src/features/digger/components.zig
@@ -1,3 +1,11 @@
 const World = @import("ecs").World;
 
+pub const Digger = struct {
+    idx_in_grid: IndexInGrid,
+
+    pub const IndexInGrid = struct {
+        r: i32,
+        c: i32,
+    };
+};
 pub const InGrid = struct { grid_entity: World.EntityID };
