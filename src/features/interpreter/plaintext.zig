3fadc2890de33c63d1c0789def0ddd810981c1db
LeviathanST <phamquochung170505@gmail.com>
2025-11-21 19:40:33 +0700

    ingame/interpreter/plaintext: use `Error`, `Command`

--- src/features/interpreter/plaintext.zig
+++ src/features/interpreter/plaintext.zig
@@ -1,60 +1,102 @@
 const std = @import("std");
 
-const ParseActionError = @import("mod.zig").ParseActionError;
-const Action = @import("mod.zig").Action;
+const Interpreter = @import("Interpreter.zig");
 
-pub fn parse(source: []const u8) !Action {
+const Error = Interpreter.Error;
+const Command = Interpreter.Command;
+
+pub fn parse(
+    alloc: std.mem.Allocator,
+    interpreter: *Interpreter,
+    source: []const u8,
+) !Command {
+    // TODO: line iteration
     const trimmed = std.mem.trim(u8, source, " ");
     var iter = std.mem.splitScalar(u8, trimmed, ' ');
 
+    var method: []const u8 = undefined;
+
     while (iter.next()) |action| {
-        inline for (std.meta.fields(Action)) |field| {
+        method = action;
+        inline for (std.meta.fields(Command)) |field| {
             if (std.mem.eql(u8, field.name, action)) {
-                const should_be_value = iter.next() orelse
-                    return ParseActionError.ActionValueNotFound;
+                const should_be_value = iter.next() orelse {
+                    try interpreter.errors.append(alloc, .{
+                        .tag = .expected_type_action,
+                        .extra = .{ .expected_token = "up/down/left/right" },
+                        .token = "",
+                    });
+                    return .none;
+                };
 
                 switch (@typeInfo(field.type)) {
                     .@"enum" => {
+                        // TODO: handle check enum values
                         const enum_value = std.meta.stringToEnum(
                             field.type,
                             should_be_value,
                         );
                         if (enum_value) |v| {
-                            return @unionInit(Action, field.name, v);
+                            return @unionInit(Command, field.name, v);
                         } else {
-                            return ParseActionError.ActionValueNotFound;
+                            try interpreter.errors.append(alloc, .{
+                                .tag = .expected_type_action,
+                                .extra = .{ .expected_token = "up/down/left/right" },
+                                .token = should_be_value,
+                            });
+                            return .none;
                         }
                     },
                     else => {
                         const T = field.type;
-                        std.log.err("Expected `Enum`, found {s} - original type - {t}", .{
-                            @typeName(T),
-                            @typeInfo(T),
+                        try interpreter.errors.append(alloc, .{
+                            .tag = .not_supported_type,
+                            .token = @typeName(T),
                         });
-                        return ParseActionError.UnsupprotedType;
+                        return .none;
                     },
                 }
             }
         }
     }
 
-    return ParseActionError.ActionNotFound;
+    try interpreter.errors.append(
+        alloc,
+        .{ .tag = .unknown_action, .token = method },
+    );
+    return .none;
 }
 
 test "parse action (plaintext)" {
+    // simulate the `World.arena`
+    var base_alloc = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer base_alloc.deinit();
+    const alloc = base_alloc.allocator();
+
+    var interpreter: Interpreter = .{};
+    const list_err = &interpreter.errors;
+
     const action_1 = "move up";
-    const parsed_1 = try parse(action_1);
-    const result_1: Action = .{
+    const parsed_1 = try parse(alloc, &interpreter, action_1);
+    const result_1: Command = .{
         .move = .up,
     };
-
     try std.testing.expectEqual(parsed_1, result_1);
 
-    const not_found = "not_found";
-    const parsed_2 = parse(not_found);
-    try std.testing.expectError(ParseActionError.ActionNotFound, parsed_2);
+    const action_2 = "nothing";
+    _ = try parse(alloc, &interpreter, action_2);
 
-    const value_not_found = "move";
-    const parsed_4 = parse(value_not_found);
-    try std.testing.expectError(ParseActionError.ActionValueNotFound, parsed_4);
+    try std.testing.expectEqual(list_err.*.getLast(), Error{
+        .tag = .unknown_action,
+        .token = "nothing",
+    });
+
+    const action_3 = "move forward";
+    _ = try parse(alloc, &interpreter, action_3);
+
+    try std.testing.expectEqualDeep(list_err.*.getLast(), Error{
+        .tag = .expected_type_action,
+        .extra = .{ .expected_token = "up/down/left/right" },
+        .token = "forward",
+    });
 }
