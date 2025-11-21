3ca29539fde7cd05eba3aa98df405a110dcc66d5
LeviathanST <phamquochung170505@gmail.com>
2025-11-21 19:38:13 +0700

    ingame/terminal: remove the space of enter, use `Interpreter`

--- src/features/terminal/systems/input.zig
+++ src/features/terminal/systems/input.zig
@@ -1,9 +1,9 @@
 const std = @import("std");
 const rl = @import("raylib");
 const digger = @import("../../digger/mod.zig");
-const interpreter = @import("../../interpreter/mod.zig");
 const utils = @import("../utils.zig");
 
+const Interpreter = @import("../../interpreter/Interpreter.zig");
 const World = @import("ecs").World;
 
 pub fn scan(
@@ -32,34 +32,43 @@ pub fn scan(
             const count_v = count.*;
             // NOTE: remove the space of `ENTER`
             const count_zero = utils.skipChar(out[0..@intCast(count_v)], &[_]u8{0});
-            const count_cr = utils.skipChar(out[0..@intCast(count_v - count_zero)], "\r");
+            var count_cr = utils.skipChar(out[0..@intCast(count_v - count_zero)], "\r\n");
+
+            const total = count_zero + count_cr;
 
             ts_backspace.* = std.time.microTimestamp();
             count.* -= blk: {
                 if (count_zero != 0 or count_cr != 0) {
-                    break :blk count_zero + count_cr;
+                    break :blk total;
                 } else {
                     break :blk 1;
                 }
             };
-            out[@intCast(count.*)] = 0;
+            if (count_cr > 0) {
+                while (count_cr > 0) : (count_cr -= 1) {
+                    out[@intCast(count_v - count_zero)] = 0;
+                }
+            } else {
+                out[@intCast(count.*)] = 0;
+            }
         }
 
         if (rl.isKeyDown(.enter)) {
             ts_backspace.* = std.time.microTimestamp();
             const remaning_to_new_line = width - @mod(count.*, width);
             out[@intCast(count.*)] = '\r';
+            out[@intCast(count.* + 1)] = '\n';
             count.* += remaning_to_new_line;
         }
     }
 }
 
-pub fn process(w: *World, alloc: std.mem.Allocator, content: [:0]const u8) !void {
-    std.log.debug("Input: {s}", .{content});
-    // TODO: handle error
-    if (try interpreter.parse(alloc, content, .zig)) |action| {
-        switch (action) {
-            .move => |direction| try digger.action.control(w, direction),
-        }
+pub fn process(w: *World, alloc: std.mem.Allocator, content: []const u8) !void {
+    var interpreter: Interpreter = .{};
+
+    const action = try interpreter.parse(alloc, content, .plaintext);
+    switch (action) {
+        .move => |direction| try digger.action.control(w, direction),
+        .none => {},
     }
 }
